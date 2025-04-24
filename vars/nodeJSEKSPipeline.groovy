898898af69bafcc7d15ebd8a9c3ee48aa119f22a
Lakshmanp1 <laxman.gdn@gmail.com>
2025-04-24 18:21:13 +0530

    changed inside the vars file name

--- vars/nodeJSEKSPipeline.groovy
+++ vars/nodeJSEKSPipeline.groovy
@@ -1,87 +1,89 @@
 def call(Map configMap){
     pipeline {
-    agent {
-        label 'AGENT-1'
-    }
-    options{
-        timeout(time: 30, unit: 'MINUTES')
-    }
-    parameters{
-        booleanParam(name: 'deploy', defaultValue: false, description: 'select to deploy or not')
-    }
-    environment {
-        appVersion = '' // This will become global, we can use env accross stages
-        region = 'us-east-1'
-        account_id = '503561459301'
-        project = configMap.get("project")
-        environment = 'dev'
-        component = configMap.get("component") 
-    }
+        agent {
+            label 'AGENT-1'
+        }
+        options{
+            timeout(time: 30, unit: 'MINUTES')
+        }
+        parameters{
+            booleanParam(name: 'deploy', defaultValue: false, description: 'select to deploy or not')
+        }
+        environment {
+            appVersion = '' // This will become global, we can use env accross stages
+            region = 'us-east-1'
+            account_id = '503561459301'
+            project = configMap.get("project")
+            environment = 'dev'
+            component = configMap.get("component") 
+        }
 
-    stages {
+        stages {
 
-        stage('Read the version') {
-            steps {
-                script{
-                    def packageJson = readJSON file: 'package.json'
-                    appVersion = packageJson.version
-                    echo "App version: ${appVersion}"
+            stage('Read the version') {
+                steps {
+                    script{
+                        def packageJson = readJSON file: 'package.json'
+                        appVersion = packageJson.version
+                        echo "App version: ${appVersion}"
+                    }
                 }
             }
-        }
-        stage('Debug Env') {
-            steps {
-                sh '''
-            echo "== Node location =="
-            which node || echo "node not found"
-            echo "== NPM location =="
-            which npm || echo "npm not found"
-            echo "== Versions =="
-            node -v || echo "node version not found"
-            npm -v || echo "npm version not found"
-            echo "== PATH =="
-            echo $PATH
-        '''
+            stage('Debug Env') {
+                steps {
+                    sh '''
+                echo "== Node location =="
+                which node || echo "node not found"
+                echo "== NPM location =="
+                which npm || echo "npm not found"
+                echo "== Versions =="
+                node -v || echo "node version not found"
+                npm -v || echo "npm version not found"
+                echo "== PATH =="
+                echo $PATH
+            '''
+                }
             }
-        }
-        stage('Install dependencies') {
-            steps {
-                sh 'npm install'
+            stage('Install dependencies') {
+                steps {
+                    sh 'npm install'
+                }
             }
-        }
-        stage('Docker build') {
-            // when{
-            //     expression {params.deploy}
-            // }
-            steps {
-                withAWS(region: 'us-east-1', credentials: "aws-creds-${environment}"){
+            stage('Docker build') {
+                // when{
+                //     expression {params.deploy}
+                // }
+                steps {
+                    withAWS(region: 'us-east-1', credentials: "aws-creds-${environment}"){
+                    
+                    sh """
+                    aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-east-1.amazonaws.com
+
+                    docker build -t ${account_id}.dkr.ecr.us-east-1.amazonaws.com/${project}/${environment}/${component}:${appVersion} .
+
+                    docker images
+
+                    docker push ${account_id}.dkr.ecr.us-east-1.amazonaws.com/${project}/${environment}/${component}:${appVersion}
                 
-                sh """
-                aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-east-1.amazonaws.com
+                    """
+                }
+                
+            }
+            stage('Deploy'){
+                    when{
+                        expression {params.deploy}
+                    }
 
-                docker build -t ${account_id}.dkr.ecr.us-east-1.amazonaws.com/${project}/${environment}/${component}:${appVersion} .
-
-                 docker images
-
-                docker push ${account_id}.dkr.ecr.us-east-1.amazonaws.com/${project}/${environment}/${component}:${appVersion}
-               
-                """
+                    steps{
+                        build job: "../${component}-cd", parameters: [
+                            string(name: 'version', value: "$appVersion"),
+                            string(name: 'ENVIRONMENT', value: "dev"),
+                        ], wait: true
+                    }
+                }
+                
             }
-            }
-        }
-        stage('Deploy'){
-             when{
-             expression {params.deploy}
-            }
-                steps{
-                build job: "../${component}-cd", parameters: [
-                    string(name: 'version', value: "$appVersion"),
-                    string(name: 'ENVIRONMENT', value: "dev"),
-                    ], wait: true
-            }
-        }
-            
-    }
+        }  
         post {
             always {
                 echo "this section runs always"
@@ -92,6 +94,7 @@ def call(Map configMap){
             }
             failure{
                 echo "this section runs when pipeline failure"
+                
             }
         }
     }
