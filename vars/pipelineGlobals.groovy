186aa75f413d730da4656e22b7a09f20cc6ebfb4
Lakshmanp1 <laxman.gdn@gmail.com>
2025-04-24 17:51:20 +0530

    changed inside the vars file name

--- vars/pipelineGlobals.groovy
+++ vars/pipelineGlobals.groovy
@@ -4,6 +4,10 @@ def getAccountID(String environment){
             return "503561459301"
         case 'qa':
             return "503561459301"
+        case 'uat':
+            return "503561459301"
+        case 'pre-prod':
+            return "503561459301"
         case 'prod':
             return "503561459301"
         default:
