d3b4888695ccb076f526acb99161110bdfc64dc0
Lakshmanp1 <laxman.gdn@gmail.com>
2025-04-25 18:29:10 +0530

    hjl

--- vars/javaEKSPipeline.groovy
+++ vars/javaEKSPipeline.groovy
@@ -63,7 +63,6 @@ def call(Map configMap){
                         aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-east-1.amazonaws.com
 
                         docker build -t ${account_id}.dkr.ecr.us-east-1.amazonaws.com/${project}/${environment}/${component}:${appVersion} .
-
                         docker images
 
                         docker push ${account_id}.dkr.ecr.us-east-1.amazonaws.com/${project}/${environment}/${component}:${appVersion}
--- vars/pythonEKSPipeline.groovy
+++ vars/pythonEKSPipeline.groovy
@@ -24,7 +24,7 @@ def call(Map configMap){
             stage('Read the version') {
                 steps {
                     script{
-                        def packageJson = readJSON file: 'requirements.txt'
+                        def packageJson = readJSON file: 'payment/package.json'
                         appVersion = packageJson.version
                         echo "App version: ${appVersion}"
                     }
